8a6fdac78954fea677f65728de7641e6dad53a54
Swathi01~ <belide1409@gmail.com>
2023-10-14 19:30:03 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="mailto:belide1409@gmail.com">belide1409@gmail.com</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/belide-lakshmi-swathi-7b4a15249/" target="_blank" rel="noopener noreferrer">click here</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/BLSwathi" target="_blank" rel="noopener noreferrer">click here</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/Education.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section>
+                <h2>Education</h2>
+            
+                <h3>Sri Chaitanya Techno School, Talangana</h3>
+                <p>10th standard</p>
+                <p>Graduation Year: 2019</p>
+            
+            
+                <h3>Trividyaa Junior College, Talangana</h3>
+                <p>Intermediate</p>
+                <p>Graduation Year: 2021</p>
+            
+            
+                <h3>Vellore Institute of Technology, Vellore</h3>
+                <p>B.tech (Computer Science Engineering with specialization in Data Science)</p>
+                <p>Graduation Year: 2025</p>
+            
+            
+    </section>
+  );
+}
+
+export default Education;
