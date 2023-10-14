37cd1e58c892b4cd33ffef207016a09a198b7b9e
Swathi01~ <belide1409@gmail.com>
2023-10-15 00:10:47 +0530

    updated

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,18 +5,18 @@ const Education = () => {
     <section>
                 <h2>Education</h2>
             
-                <h3>Sri Chaitanya Techno School, Talangana</h3>
+                <h3>Sri Chaitanya Techno School, Telangana</h3>
                 <p>10th standard</p>
                 <p>Graduation Year: 2019</p>
             
             
-                <h3>Trividyaa Junior College, Talangana</h3>
+                <h3>Trividyaa Junior College, Telangana</h3>
                 <p>Intermediate</p>
                 <p>Graduation Year: 2021</p>
             
             
                 <h3>Vellore Institute of Technology, Vellore</h3>
-                <p>B.tech (Computer Science Engineering with specialization in Data Science)</p>
+                <p>B.tech (Computer Science Engineering with specialization in Bioinformation)</p>
                 <p>Graduation Year: 2025</p>
             
             
